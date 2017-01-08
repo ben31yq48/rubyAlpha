cdcb07ae14549c4eb1beb3e18d11f966da7dec93
ntu_ror_ubuntu <ben31yq48@yahoo.com.tw>
2017-01-07 22:11:39 -0800

    second commit

---
+++ lesson1/circle_cal.rb
@@ -0,0 +1,3 @@
+def area_of_circle(radius)
+    return radius * radius * 3.14
+end
---
+++ lesson1/hello_world.rb
@@ -0,0 +1,23 @@
+puts "hello world"
+
+str1 = "this is a test"
+str2 = " of the automatic pager system"
+
+str1 + str2
+
+puts str1 + str2
+
+puts "#{str1}"
+puts "#{str1}#{str2}"
+
+number_str1 = "5"
+number_str2 = "2"
+
+puts number_str1 + number_str2
+
+puts number_str1.to_i + number_str2.to_i
+
+number_str1 = number_str1.to_i
+number_str2 = number_str2.to_i
+
+puts number_str1 + number_str2
